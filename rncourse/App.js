4b3ec281a00b40c2773c0d8e98580350a3e07dbb
Kevin <kevsalcedo@gmail.com>
2019-04-25 16:38:43 -0500

    up to first exercise in basics react native course

---
+++ rncourse/App.js
@@ -0,0 +1,107 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from "react-native";
+import ListItem from "./src/components/ListItem/ListItem";
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    placeName: "",
+    places: []
+  };
+
+  placeNameChangedHandler = text => {
+    this.setState({ placeName: text });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
+
+  render() {
+    const placesOutput = this.state.places.map((place, i) => {
+      return <ListItem key={i} placeName={place} />;
+    });
+    return (
+      <View style={styles.container}>
+        <View style={styles.inline}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="Add text here"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+            underlineColorAndroid="blue"
+          />
+          <Button
+            title="Add"
+            styles={styles.placeButton}
+            onPress={this.placeSubmitHandler}
+          />
+        </View>
+        <View style={styles.listContainer}>{placesOutput}</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  },
+  inline: {
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  },
+  listContainer: {
+    width: "75%"
+  }
+});
---
+++ rncourse/src/components/ListItem/ListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default props => (
+  <View style={styles.listItem}>
+    <Text>{props.placeName}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  listItem: {
+    width: "100%",
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#eee",
+    alignItems: "center"
+  }
+});
