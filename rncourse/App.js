fe00eb0c830d28a8f3f0b99c9ce00922114c2006
Kevin <kevsalcedo@gmail.com>
2019-04-30 11:35:54 -0500

    functioning starter app with react native navigation. created auth screen

--- rncourse/App.js
+++ rncourse/App.js
@@ -1,84 +1,95 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-import { connect } from "react-redux";
+import { Navigation } from "react-native-navigation";
+import AuthScreen from "./src/screens/AuthScreen";
 
-import UserInput from "./src/components/UserInput";
-import PlacesList from "./src/components/PlacesList";
-import PlaceDetail from "./src/components/PlaceDetail";
-
-import { appStyles as styles } from "./src/styles/appStyles";
-import lagoon from "./src/assets/lagoon.jpg";
-
-import {
-  addPlace,
-  deletePlace,
-  selectPlace,
-  deselectPlace
-} from "./src/store/actions";
-
-type Props = {};
-class App extends Component<Props> {
-  state = {
-    placeName: ""
-  };
-
-  placeNameChangedHandler = text => {
-    this.setState({ placeName: text });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-    this.placeAddedhandler(this.state.placeName);
-  };
-
-  placeAddedhandler = placeName => {
-    this.props.addPlace(placeName);
-  };
-
-  placeSelectedHandler = key => {
-    this.props.selectPlace(key);
-  };
-
-  onItemDeletedHandler = () => {
-    this.props.deletePlace();
-  };
-
-  onModalClosedHandler = () => {
-    this.props.deselectPlace();
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail
-          selectedPlace={this.props.place}
-          onItemDeleted={this.onItemDeletedHandler}
-          onModalClosed={this.onModalClosedHandler}
-        />
-        <UserInput
-          placeName={this.state.placeName}
-          placeNameChangedHandler={this.placeNameChangedHandler}
-          placeSubmitHandler={this.placeSubmitHandler}
-        />
-        <PlacesList
-          places={this.props.places}
-          onItemSelected={this.placeSelectedHandler}
-        />
-      </View>
-    );
+// Register Screens
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen); // Start a App
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
   }
-}
+});
 
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    place: state.places.place
-  };
-};
+// import React, { Component } from "react";
+// import { View } from "react-native";
+// import { connect } from "react-redux";
 
-export default connect(
-  mapStateToProps,
-  { addPlace, deletePlace, selectPlace, deselectPlace }
-)(App);
+// import UserInput from "./src/components/UserInput";
+// import PlacesList from "./src/components/PlacesList";
+// import PlaceDetail from "./src/components/PlaceDetail";
+
+// import { appStyles as styles } from "./src/styles/appStyles";
+// import lagoon from "./src/assets/lagoon.jpg";
+
+// import {
+//   addPlace,
+//   deletePlace,
+//   selectPlace,
+//   deselectPlace
+// } from "./src/store/actions";
+
+// class App extends Component {
+//   state = {
+//     placeName: ""
+//   };
+
+//   placeNameChangedHandler = text => {
+//     this.setState({ placeName: text });
+//   };
+
+//   placeSubmitHandler = () => {
+//     if (this.state.placeName.trim() === "") {
+//       return;
+//     }
+//     this.placeAddedhandler(this.state.placeName);
+//   };
+
+//   placeAddedhandler = placeName => {
+//     this.props.addPlace(placeName);
+//   };
+
+//   placeSelectedHandler = key => {
+//     this.props.selectPlace(key);
+//   };
+
+//   onItemDeletedHandler = () => {
+//     this.props.deletePlace();
+//   };
+
+//   onModalClosedHandler = () => {
+//     this.props.deselectPlace();
+//   };
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <PlaceDetail
+//           selectedPlace={this.props.place}
+//           onItemDeleted={this.onItemDeletedHandler}
+//           onModalClosed={this.onModalClosedHandler}
+//         />
+//         <UserInput
+//           placeName={this.state.placeName}
+//           placeNameChangedHandler={this.placeNameChangedHandler}
+//           placeSubmitHandler={this.placeSubmitHandler}
+//         />
+//         <PlacesList
+//           places={this.props.places}
+//           onItemSelected={this.placeSelectedHandler}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     places: state.places.places,
+//     place: state.places.place
+//   };
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   { addPlace, deletePlace, selectPlace, deselectPlace }
+// )(App);
--- rncourse/index.js
+++ rncourse/index.js
@@ -1,20 +1,20 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import React from "react";
-import { AppRegistry } from "react-native";
-import { Provider } from "react-redux";
+// import React from "react";
+// import { AppRegistry } from "react-native";
+// import { Provider } from "react-redux";
 import App from "./App";
-import { name as appName } from "./app.json";
-import configureStore from "./src/store/configureStore";
+// import { name as appName } from "./app.json";
+// import configureStore from "./src/store/configureStore";
 
-const store = configureStore();
+// const store = configureStore();
 
-const WrappedApp = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+// const WrappedApp = () => (
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
 
-AppRegistry.registerComponent(appName, () => WrappedApp);
+// AppRegistry.registerComponent(appName, () => WrappedApp);
