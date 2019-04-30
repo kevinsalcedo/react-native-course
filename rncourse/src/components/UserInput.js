94feb61a3799e9a33bbf21515c105c26bd6d40c5
Kevin <kevsalcedo@gmail.com>
2019-04-30 12:05:41 -0500

    refactored UserInput to match course

--- rncourse/src/components/UserInput.js
+++ rncourse/src/components/UserInput.js
@@ -2,21 +2,44 @@ import React from "react";
 import { View, TextInput, Button } from "react-native";
 import { userInputStyles as styles } from "../styles/userInputStyles";
 
-export default props => {
-  return (
-    <View style={styles.inline}>
-      <TextInput
-        style={styles.placeInput}
-        value={props.placeName}
-        placeholder="Add your favorite place here"
-        onChangeText={props.placeNameChangedHandler}
-        underlineColorAndroid="black"
-      />
-      <Button
-        title="Add"
-        styles={styles.placeButton}
-        onPress={props.placeSubmitHandler}
-      />
-    </View>
-  );
-};
+class UserInput extends Component {
+  state = {
+    placeName: ""
+  };
+
+  componentDidMount() {}
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.props.onPlaceAdded(this.state.placeName);
+  };
+
+  render() {
+    return (
+      <View style={styles.inline}>
+        <TextInput
+          placeholder="An awesome place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+          style={styles.placeInput}
+        />
+        <Button
+          title="Add"
+          style={styles.placeButton}
+          onPress={this.placeSubmitHandler}
+        />
+      </View>
+    );
+  }
+}
+
+export default UserInput;
