490a4bdd57387e3541935c64891e21afc234d8d9
Kevin <kevsalcedo@gmail.com>
2019-05-01 09:57:10 -0500

    created a default styled component and synthetic cascading of styles

--- rncourse/src/screens/Auth.js
+++ rncourse/src/screens/Auth.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import DefaultInput from "../components/UI/DefaultInput";
 import startMainTabs from "./startMainTabs";
 
 class AuthScreen extends React.Component {
@@ -9,12 +10,32 @@ class AuthScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler} />
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title="Switch to Login" />
+        <View style={styles.inputContainer}>
+          <DefaultInput placeholder="Your Email Address" style={styles.input} />
+          <DefaultInput placeholder="Password" style={styles.input} />
+          <DefaultInput placeholder="Confirm Password" style={styles.input} />
+        </View>
+        <Button title="Submit" onPress={this.loginHandler} />
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#bbb"
+  }
+});
 export default AuthScreen;
