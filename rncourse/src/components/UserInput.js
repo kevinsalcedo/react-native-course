f957ae138f9c213181abcb2292c824ff7dba3dbe
Kevin <kevsalcedo@gmail.com>
2019-05-01 14:26:07 -0500

    refactored share place into multiple components

--- rncourse/src/components/UserInput.js
+++ rncourse/src/components/UserInput.js
@@ -1,45 +1,14 @@
 import React from "react";
-import { View, TextInput, Button } from "react-native";
-import { userInputStyles as styles } from "../styles/userInputStyles";
+import DefaultInput from "../components/UI/DefaultInput";
 
-class UserInput extends React.Component {
-  state = {
-    placeName: ""
-  };
-
-  componentDidMount() {}
-
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
-    this.props.onPlaceAdded(this.state.placeName);
-  };
-
-  render() {
-    return (
-      <View style={styles.inline}>
-        <TextInput
-          placeholder="An awesome place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-          style={styles.placeInput}
-        />
-        <Button
-          title="Add"
-          style={styles.placeButton}
-          onPress={this.placeSubmitHandler}
-        />
-      </View>
-    );
-  }
-}
+const UserInput = props => {
+  return (
+    <DefaultInput
+      placeholder="Place Name"
+      value={props.placeName}
+      onChangeText={props.onChangeText}
+    />
+  );
+};
 
 export default UserInput;
--- rncourse/src/screens/SharePlace.js
+++ rncourse/src/screens/SharePlace.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  ScrollView,
+  Image
+} from "react-native";
 import { connect } from "react-redux";
 import { addPlace } from "../store/actions";
 
 import UserInput from "../components/UserInput";
+import MainText from "../components/UI/MainText";
+import HeadingText from "../components/UI/HeadingText";
+import lagoon from "../assets/lagoon.jpg";
+import PickImage from "../components/PickImage";
+import PickLocation from "../components/PickLocation";
 
 class SharePlace extends React.Component {
+  state = {
+    placeName: ""
+  };
   constructor(props) {
     super(props);
     this.props.navigator.setOnNavigatorEvent(this.setOnNavigatorEvent);
@@ -21,17 +37,61 @@ class SharePlace extends React.Component {
     }
   };
 
-  placeAddedHandler = name => {
-    this.props.addPlace(name);
+  placeAddedHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.props.addPlace(this.state.placeName);
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
   };
   render() {
     return (
-      <View>
-        <UserInput onPlaceAdded={this.placeAddedHandler} />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a Place With Us!</HeadingText>
+          </MainText>
+          <PickImage />
+          <PickLocation />
+          <UserInput
+            placeName={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <View style={styles.button}>
+            <Button title="Share" onPress={this.placeAddedHandler} />
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  placeholder: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#eee",
+    width: "80%",
+    height: 150
+  },
+  button: {
+    margin: 8
+  },
+  previewImage: {
+    width: "100%",
+    height: "100%"
+  }
+});
+
 export default connect(
   null,
   { addPlace }
