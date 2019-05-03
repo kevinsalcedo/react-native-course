4f03e7e4d6d6a7d57d23c4fbae2449ac14212f4b
Kevin <kevsalcedo@gmail.com>
2019-05-03 15:46:32 -0500

    add marker on map press

--- rncourse/src/components/PickLocation.js
+++ rncourse/src/components/PickLocation.js
@@ -11,15 +11,38 @@ class PickLocation extends React.Component {
       longitudeDelta:
         (Dimensions.get("window").width / Dimensions.get("window").height) *
         0.0122
-    }
+    },
+    locationChosen: false
+  };
+
+  handleMapPress = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        },
+        locationChosen: true
+      };
+    });
   };
   render() {
+    let marker = null;
+
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+    }
     return (
       <View style={styles.container}>
         <MapView
-          initialRegion={this.state.focusedLocation}
+          region={this.state.focusedLocation}
           style={styles.map}
-        />
+          onPress={this.handleMapPress}
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
           <Button title="Locate Me" onPress={() => alert("Locate Me")} />
         </View>
