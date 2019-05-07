9e9e1b37b7ee1274f2623fac0e215573886f6094
Kevin <kevsalcedo@gmail.com>
2019-05-07 13:46:09 -0500

    installed firebase project

--- rncourse/src/store/actions/places.js
+++ rncourse/src/store/actions/places.js
@@ -1,13 +1,26 @@
 import { ADD_PLACE, DELETE_PLACE } from "./types";
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    payload: {
-      placeName,
-      location,
-      image
-    }
+  // return {
+  //   type: ADD_PLACE,
+  //   payload: {
+  //     placeName,
+  //     location,
+  //     image
+  //   }
+  // };
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch("https://react-native-1556907249873.firebaseio.com/places.json", {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+      .catch(err => alert("Failed", err))
+      .then(res => res.json())
+      .then(parsedRes => {});
   };
 };
 
--- rncourse/src/store/configureStore.js
+++ rncourse/src/store/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducers from "./reducers";
 
+let composeEnhancers = compose;
+
 const configureStore = () => {
-  return createStore(reducers, {});
+  return createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
