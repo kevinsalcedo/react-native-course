f17fbd3508d8ca8aaa1e0b9f258677327789dd82
Kevin <kevsalcedo@gmail.com>
2019-05-07 15:00:17 -0500

    added ability to store images on firebase

--- rncourse/src/store/actions/places.js
+++ rncourse/src/store/actions/places.js
@@ -10,14 +10,26 @@ export const addPlace = (placeName, location, image) => {
   //   }
   // };
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location: location
-    };
-    fetch("https://react-native-1556907249873.firebaseio.com/places.json", {
-      method: "POST",
-      body: JSON.stringify(placeData)
-    })
+    // const placeData = {
+    //   name: placeName,
+    //   location: location
+    // };
+    // fetch("https://react-native-1556907249873.firebaseio.com/places.json", {
+    //   method: "POST",
+    //   body: JSON.stringify(placeData)
+    // })
+    //   .catch(err => alert("Failed", err))
+    //   .then(res => res.json())
+    //   .then(parsedRes => {});
+    fetch(
+      "https://us-central1-react-native-1556907249873.cloudfunctions.net/storeImage",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
       .catch(err => alert("Failed", err))
       .then(res => res.json())
       .then(parsedRes => {});
